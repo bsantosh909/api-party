5a31bb19789478fb3964534dbb02ec5a266933bb
Santosh Bhandari <bsantosh909@gmail.com>
2020-11-04 11:08:57 +0545

    Add supercell games related API

--- assets/apiList.js
+++ assets/apiList.js
@@ -1,1 +1,33 @@
-export const apiList = [];
+export const apiList = [
+	{
+		id: "coc.supercell",
+		name: "Clash of Clans API",
+		description: "Official Clash of clans API",
+		apiUrl: "https://api.clashofclans.com/v1/",
+		docs: "https://developer.clashofclans.com/",
+		official: true
+	},
+	{
+		id: "cr.supercell",
+		name: "Clash Royale API",
+		description: "Official Clash Royale API",
+		apiUrl: "https://api.clashroyale.com/v1/",
+		docs: "https://developer.clashroyale.com/",
+		official: true
+	},
+	{
+		id: "bs.supercell",
+		name: "Brawl Stars API",
+		description: "Official Brawl Stars API",
+		apiUrl: "https://api.brawlstars.com/v1/",
+		docs: "https://developer.brawlstars.com/",
+		official: true
+	},
+	{
+		id: "starlist",
+		name: "Starlist Brawl Stars",
+		description: "Brawl Stars API to provide assets, logs and lots of useful data.",
+		apiUrl: "https://api.starlist.pro",
+		docs: "https://www.starlist.pro/dashboard/"
+	}
+];
