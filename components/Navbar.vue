f5392cfd74e40a9b73a449d9c219f778c8740508
Santosh Bhandari <bsantosh909@gmail.com>
2020-11-05 15:33:26 +0545

    Sticky navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,5 +1,7 @@
 <template>
-	<nav class="bg-gray-800 text-gray-100 p-3 text-center font-bold">
+	<nav
+		class="sticky top-0 z-50 bg-gray-800 text-gray-100 p-3 text-center font-bold"
+	>
 		<nuxt-link to="/">API PARTY</nuxt-link>
 	</nav>
 </template>
