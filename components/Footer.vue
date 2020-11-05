e4613cfd008248e55b5d19223130f4a9bacbabc0
Santosh Bhandari <bsantosh909@gmail.com>
2020-11-05 15:25:32 +0545

    Link to personal site

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,14 @@
 <template>
 	<div class="bg-gray-800 text-gray-100 p-4">
-		<div class="text-center">Copyright © 2020 Santosh Bhandari</div>
+		<div class="text-center">
+			<span>Copyright © 2020 </span>
+			<a
+				href="https://santoshb.com.np?ref=apiparty"
+				target="_blank"
+				class="text-yellow-200"
+			>
+				Santosh Bhandari
+			</a>
+		</div>
 	</div>
 </template>
