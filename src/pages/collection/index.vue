4e654ea54eb94276ee7a35be36910ffc3ddb58fc
abhijeet007rocks8 <abhijeet007rocks8@gmail.com>
2022-10-17 12:50:49 +0530

    external param to nuxtlink

--- src/pages/collection/index.vue
+++ src/pages/collection/index.vue
@@ -135,6 +135,7 @@
                 target="_blank"
                 class="group"
                 :prefetch="false"
+                external="true"
               >
                 <div class="flex gap-x-2">
                   <h2 class="text-xl font-semibold mb-2 group-hover:underline">
